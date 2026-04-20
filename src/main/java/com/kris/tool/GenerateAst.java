fd021e84f21984d1494b80c5b8ae796441ba1951
Kris <kris1toja@gmail.com>
2026-04-20 17:52:48 +0300

    [FEAT] implemented visitor pattern

--- src/main/java/com/kris/tool/GenerateAst.java
+++ src/main/java/com/kris/tool/GenerateAst.java
@@ -13,7 +13,7 @@ public class GenerateAst {
         }
         String outputDir = args[0];
 
-        defineAst(outputDir, "Expr", Arrays.asList(
+        defineAst(outputDir, "Expr:", Arrays.asList(
                 "Binary : Expr left, Token operator, Expr right",
                 "Grouping : Expr expression",
                 "Literal : Object value",
@@ -30,6 +30,12 @@ public class GenerateAst {
         writer.println("import java.util.List;");
         writer.println();
         writer.println("abstract class " + baseName + " {");
+
+        defineVisitor(writer, baseName, types);
+
+        writer.println();
+        writer.println("  abstract <R> R accept(Visitor<R> visitor);");
+        writer.println();
 
         for (String type : types){
             String className = type.split(":")[0].trim();
@@ -54,10 +60,27 @@ public class GenerateAst {
         writer.println("    }");
 
         writer.println();
+        writer.println("    @Override");
+        writer.println("    <R> R accept(Visitor<R> visitor) {");
+        writer.println("     return visitor.visit" + className + baseName + "(this);");
+
+        writer.println("    }");
+
+        writer.println();
         for(String field : fields) {
             writer.println("    final " + field + ";");
         }
 
         writer.println(" }");
+    }
+
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types){
+        writer.println(" interface Visitor<R> {");
+
+        for(String type:types){
+            String typeName = type.split(":")[0].trim();
+            writer.println("  R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
+        }
+        writer.println("  }");
     }
 }
